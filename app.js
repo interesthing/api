e286c30878f0e03bc8f3496346d90646c844d9d7
Flecto <flecto@macbook-pro-de-flecto.home>
2019-11-15 22:41:26 +0100

    update app

--- app.js
+++ app.js
@@ -31,8 +31,8 @@ if (process.env.NODE_ENV !== 'test') {
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'docs')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
